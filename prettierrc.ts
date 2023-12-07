e9f4e8615b33de1eb9a1fddc1969abda4eba5f6d
Your Name <zai-elina>
2023-12-07 09:44:35 +0300

    Add eslint and prettier

---
+++ prettierrc.ts
@@ -0,0 +1,16 @@
+module.exports = {
+  bracketSpacing: false,
+  singleQuote: true,
+  bracketSameLine: true,
+  trailingComma: "es5",
+  printWidth: 80,
+  parser: "flow",
+  arrowParens: "avoid",
+  overrides: [
+    {
+      options: {
+        trailingComma: "all",
+      },
+    },
+  ],
+};
