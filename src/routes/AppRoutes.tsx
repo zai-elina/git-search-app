755ca6f29e67efe617f27ad4871a6dc6752965dd
Элина <@COMPUTER>
2023-12-15 22:14:39 +0300

    Change Readme and create page 404

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -2,12 +2,14 @@ import React, { FC } from "react";
 import { Route, Routes } from "react-router-dom";
 import MainPage from "../pages/mainPage/MainPage";
 import AboutUserPage from "../pages/aboutUserPage/AboutUserPage";
+import { NotFoundPage } from "../pages/notFoundPage/NotFoundPage";
 
 const AppRoutes: FC = () => {
   return (
     <Routes>
       <Route path="/" element={<MainPage />} />
       <Route path="/users/:login" element={<AboutUserPage />} />
+      <Route path="/*" element={<NotFoundPage />} />
     </Routes>
   );
 };
